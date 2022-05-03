76afddcc64c3d289e893d13158bd3a0dbd516171
Alícia Rodrigues <rodriguesf.alicia@gmail.com>
2022-05-03 14:50:36 -0300

    Initial commit

---
+++ Todoey/Views/CategoryViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryViewCell.swift
+//  Todoey
+//
+//  Created by Alicia Rodrigues  on 27/04/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
